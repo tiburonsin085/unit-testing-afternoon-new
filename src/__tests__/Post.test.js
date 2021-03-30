ce68a49a438c5ffaf80cccfc308ebc10992cbca2
Esteban Portillo <portilloesteban1@gmail.com>
2021-03-30 12:24:47 -0400

    not finished yet

---
+++ src/__tests__/Post.test.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import {render, act} from '@testing-library/react'
+import Post from '../views/Post'
+import axios from 'axios'
+import {MemoryRouter} from 'react-router-dom'
+import { posts } from './__data__/testData'
